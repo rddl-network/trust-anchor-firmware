cd39b14de3de5902025052b20f2983fe00839e7b
Stefan Weber <stefan.weber93@googlemail.com>
2023-12-04 13:54:17 +0100

    Add routes to main
    Signed-off-by: Stefan Weber <stefan.weber93@googlemail.com>

--- src/OCC/bip32/bip32.h
+++ src/OCC/bip32/bip32.h
@@ -6,7 +6,6 @@
 void routeBip32KeyInit(OSCMessage &msg, int addressOffset);
 void routeBip32KeyFromSeed(OSCMessage &msg, int addressOffset);
 void routeBip32KeyFromParent(OSCMessage &msg, int addressOffset);
-void routeBip32KeyFromParent2(OSCMessage &msg, int addressOffset);
 void routeBip32KeyToBase58(OSCMessage &msg, int addressOffset);
 void routeBip32KeyFromParentPathString(OSCMessage &msg, int addressOffset);
 void routeBip32KeySerialize(OSCMessage &msg, int addressOffset);
